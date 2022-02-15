d11b3950bdede08eeebf478c4194b2ca9b482c1f
Francesco-Ostellari <francescoostellari@gmail.com>
2022-02-15 15:38:39 +0100

    add-movie

---
+++ Movie.php
@@ -0,0 +1,102 @@
+<?php
+
+class Movie
+{
+  public $title;
+  public $actors;
+  public $scenography;
+  public $producers;
+  public $genres;
+  public $date;
+
+  public function __construct($title, $actors, $date)
+  {
+    $this->title = $title;
+    $this->actors = $actors;
+    $this->date = $date;
+  }
+
+  public function getTitle()
+  {
+    return $this->title;
+  }
+  public function setTitle($title)
+  {
+    $this->title = $title;
+  }
+
+  public function getActors()
+  {
+    return $this->actors;
+  }
+  public function setEditor($actors)
+  {
+    $this->actors = $actors;
+  }
+
+  public function getDate()
+  {
+    return $this->date;
+  }
+  public function setCover($date)
+  {
+    $this->date = $date;
+  }
+  
+  public function getScenography()
+  {
+    return $this->scenography;
+  }
+  public function setScenography($scenography)
+  {
+    $this->scenography = $scenography;
+  }
+
+  public function getProducers()
+  {
+    return $this->producers;
+  }
+
+  public function setProducers($producers)
+  {
+    $this->producers = $producers;
+  }
+
+  public function getGenres()
+  {
+    return $this->genres;
+  }
+  public function setGenres($genres)
+  {
+    $this->genres = $genres;
+  }
+
+}
+
+$movie = new Movie('Braveheart', 'Mel Gibson', '1995');
+$movie->setScenography('Thomas E. Sanders');
+$movie->setProducers('Mel Gibson');
+$movie->setGenres('Azione');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Movie</title>
+</head>
+<body>
+  <h1>MOVIE</h1>
+  <ul>
+    <li>Titolo: <?=$movie->getTitle()?></li>
+    <li>Attore: <?=$movie->getActors()?></li>
+    <li>Anno: <?=$movie->getDate()?></li>
+    <li>Scenografia: <?=$movie->getScenography()?></li>
+    <li>Regista: <?=$movie->getProducers()?></li>
+    <li>Genere: <?=$movie->getGenres()?></li>
+  </ul>
+</body>
+</html>
